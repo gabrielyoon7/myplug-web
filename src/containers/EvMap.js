090d57bc416191cfeb2eed152fafb77e1bb1b186
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-12-04 20:12:14 +0900

    test: zoom controll panel 추가

--- src/containers/EvMap.js
+++ src/containers/EvMap.js
@@ -1,7 +1,8 @@
-import { useState } from "react";
-import { Map, MapMarker } from "react-kakao-maps-sdk";
+import { useRef, useState } from "react";
+import { Map, MapMarker, ZoomControl } from "react-kakao-maps-sdk";
 import TestPanel from "./TestPanel";
 export default function EvMap() {
+    const { kakao } = window;
     const [state, setState] = useState({
         // 지도의 초기 위치
         center: { lat: 33.452613, lng: 126.570888 },
@@ -9,6 +10,8 @@ export default function EvMap() {
         isPanto: false,
     })
     const [level, setLevel] = useState(3);
+    const mapRef = useRef();
+    const [info, setInfo] = useState();
     return (
         <>
             <Map
@@ -16,15 +19,20 @@ export default function EvMap() {
                 isPanto={state.isPanto}
                 level={level} // 지도의 확대 레벨
                 style={{ width: "100%", height: "90%" }}
+                ref={mapRef}
             >
                 <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
                     <div style={{ color: "#000" }}>Hello World!</div>
                 </MapMarker>
+                <ZoomControl position={kakao.maps.ControlPosition.TOPRIGHT} />
             </Map>
             <TestPanel
                 setState={setState}
                 level={level}
                 setLevel={setLevel}
+                mapRef={mapRef}
+                info={info}
+                setInfo={setInfo}
             />
         </>
     )
--- src/containers/TestPanel.js
+++ src/containers/TestPanel.js
@@ -1,28 +1,35 @@
+import { Box, Divider } from "@mui/material"
+
 export default function TestPanel({
-    setState, level, setLevel
+    setState, level, setLevel, info, setInfo, mapRef
 }) {
     return (
-        <div>
-            <button
-                onClick={() =>
-                    setState({
-                        center: { lat: 33.452613, lng: 126.570888 },
-                        isPanto: false,
-                    })
-                }
-            >
-                지도 중심좌표 이동시키기
-            </button>
-            <button
-                onClick={() =>
-                    setState({
-                        center: { lat: 33.45058, lng: 126.574942 },
-                        isPanto: true,
-                    })
-                }
-            >
-                지도 중심좌표 부드럽게 이동시키기
-            </button>
+        <div style={{'overflow':'scroll', height: "10%"}}>
+            <div>
+                <button
+                    onClick={() =>
+                        setState({
+                            center: { lat: 33.452613, lng: 126.570888 },
+                            isPanto: false,
+                        })
+                    }
+                >
+                    지도 중심좌표 이동시키기
+                </button>
+                <button
+                    onClick={() =>
+                        setState({
+                            center: { lat: 33.45058, lng: 126.574942 },
+                            isPanto: true,
+                        })
+                    }
+                >
+                    지도 중심좌표 부드럽게 이동시키기
+                </button>
+            </div>
+            <Box my={1}>
+                <Divider />
+            </Box>
             <button
                 onClick={() =>
                     setLevel(level - 1)
@@ -38,6 +45,43 @@ export default function TestPanel({
                 지도 레벨 + 1
             </button>
             <p>현재 지도 레벨은 {level} 레벨 입니다.</p>
+            <Box my={1}>
+                <Divider />
+            </Box>
+            <button onClick={() => {
+                const map = mapRef.current
+                setInfo({
+                    center: {
+                        lat: map.getCenter().getLat(),
+                        lng: map.getCenter().getLng(),
+                    },
+                    level: map.getLevel(),
+                    typeId: map.getMapTypeId(),
+                    swLatLng: {
+                        lat: map.getBounds().getSouthWest().getLat(),
+                        lng: map.getBounds().getSouthWest().getLng(),
+                    },
+                    neLatLng: {
+                        lat: map.getBounds().getNorthEast().getLat(),
+                        lng: map.getBounds().getNorthEast().getLng(),
+                    },
+                })
+            }}>
+                정보 가져 오기!
+            </button>
+            {info && (
+                <div>
+                    <p>위도 : {info.center.lat}</p>
+                    <p>경도 : {info.center.lng}</p>
+                    <p>레벨 : {info.level}</p>
+                    <p>타입 : {info.typeId}</p>
+                    <p>남서쪽 좌표 : {info.swLatLng.lat}, {info.swLatLng.lng}</p>
+                    <p>북동쪽 좌표 : {info.neLatLng.lat}, {info.neLatLng.lng}</p>
+                </div>
+            )}
+            <Box my={1}>
+                <Divider />
+            </Box>
         </div>
     )
 }
