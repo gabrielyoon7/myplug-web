fe85bdd0a4cf621675230c5512a6b713862d3554
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-01-01 23:44:49 +0900

    feat: 충전용 마커 이미지 적용

--- src/components/CustomMarker.js
+++ src/components/CustomMarker.js
@@ -3,6 +3,7 @@ import {
 } from '@mui/material';
 import { useState } from 'react';
 import { MapMarker } from 'react-kakao-maps-sdk';
+import markerGray from '../assets/stations/ev-gray.png';
 
 function CustomMarker({ station }) {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,6 +12,19 @@ function CustomMarker({ station }) {
       position={{ lat: station.lat, lng: station.lng }}
       clickable // 마커를 클릭했을 때 지도의 클릭 이벤트가 발생하지 않도록 설정합니다
       onClick={() => setIsOpen(true)}
+      image={{
+        src: markerGray, // 마커이미지의 주소입니다
+        size: {
+          width: 25,
+          height: 25,
+        }, // 마커이미지의 크기입니다
+        options: {
+          offset: {
+            x: 27,
+            y: 69,
+          }, // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+        },
+      }}
     >
       {/* MapMarker의 자식을 넣어줌으로 해당 자식이 InfoWindow로 만들어지게 합니다 */}
       {/* 인포윈도우에 표출될 내용으로 HTML 문자열이나 React Component가 가능합니다 */}
